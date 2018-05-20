697eac26394a431a82dc0e6c49b2b060056b7633
venepe <venepe@users.noreply.github.com>
2018-05-20 11:19:12 -0500

    Initial commit

---
+++ REACT.md
@@ -0,0 +1,38 @@
+# CoreUI React version
+
+## Intro
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app)
+
+It uses Sass (with .scss). The styles are loaded at the template level with n`ode-sass-chokidar` css preprocessor
+
+Dependencies are handled by **yarn**.
+
+## Directories
+```
+CoreUI-React#v2.0.0
+├── public/      (static files)
+│   ├── assets/  (assets)
+│   └── index.html (html temlpate)
+│
+├── src/             (project root)
+│   ├── containers/  (container source)
+│   ├── scss/        (scss/css source)
+│   ├── views/       (views source)
+│   ├── App.js
+│   ├── App.test.js
+│   ├── index.js
+│   ├── _nav.js      (sidebar config)
+│   └── routes.js    (routes config)
+│
+└── package.json
+```
+
+## Usage
+`yarn` - to install dependencies
+
+## Sctipts
+`yarn start` for developing (it runs webpack-dev-server)  
+`yarn build` to run a dev build  
+
+## See also
+[Readme](./README.md)
---
+++ src/views/Dashboard/Dashboard.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { Bar, Line } from 'react-chartjs-2';
+import {
+  Badge,
+  Button,
+  ButtonDropdown,
+  ButtonGroup,
+  ButtonToolbar,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  Col,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Progress,
+  Row,
+  Table,
+} from 'reactstrap';
+import Widget03 from '../../views/Widgets/Widget03'
+import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
+import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities'
+
+const brandPrimary = getStyle('--primary')
+const brandSuccess = getStyle('--success')
+const brandInfo = getStyle('--info')
+const brandWarning = getStyle('--warning')
+const brandDanger = getStyle('--danger')
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.onRadioBtnClick = this.onRadioBtnClick.bind(this);
+
+    this.state = {
+      dropdownOpen: false,
+      radioSelected: 2,
+    };
+  }
+
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen,
+    });
+  }
+
+  onRadioBtnClick(radioSelected) {
+    this.setState({
+      radioSelected: radioSelected,
+    });
+  }
+
+  render() {
+
+    return (
+      <div className="animated fadeIn">
+        <Row>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
